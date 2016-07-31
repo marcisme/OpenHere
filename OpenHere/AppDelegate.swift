778a843c8be0762dd53e67f7c3a44abbf82cc931
Marc Schwieterman <marc@marcisme.com>
2016-07-30 18:00:17 -0700

    Create new project

---
+++ OpenHere/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OpenHere
+//
+//  Created by Marc Schwieterman on 7/30/16.
+//  Copyright © 2016 Marc Schwieterman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
